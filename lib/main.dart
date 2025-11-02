f632f267d349da6f13877f0243d83914195d1d75
Kaung Myat Thu 🇲🇲 <161342107+sutoji5@users.noreply.github.com>
2025-11-02 16:55:09 +0630

    clean up some codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movie_with_searchbar/src/app.dart';
-import 'package:movie_with_searchbar/src/screens/movie_screen.dart';
 
 void main() {
   runApp(const App());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(home: Scaffold(body: MovieScreen()));
-  }
 }
